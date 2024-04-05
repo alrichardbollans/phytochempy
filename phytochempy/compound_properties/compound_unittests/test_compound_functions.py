e42ed3ac4f6f8e48dfbd55ec315c352197ded3d3
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2024-04-05 11:06:28 +0100

    Improve cirpy tests

--- phytochempy/compound_properties/compound_unittests/test_compound_functions.py
+++ phytochempy/compound_properties/compound_unittests/test_compound_functions.py
@@ -1,34 +1,53 @@
 import unittest
-import urllib
-from unittest.mock import patch
 
-import numpy as np
-from requests.exceptions import HTTPError
+import pandas as pd
 
-from phytochempy.metabolite_properties import resolve_cas_to_smiles, simplify_inchi_key, resolve_cas_to_inchikey
+from phytochempy.compound_properties import resolve_cas_to_smiles, simplify_inchi_key, resolve_cas_to_inchikey, get_smiles_and_inchi_from_cas_ids, \
+    add_CAS_ID_translations_to_df
 
 
 class CASresolve(unittest.TestCase):
     def setUp(self):
-        self.aspirin = {'cas_id': '50-78-2', 'smiles': 'CC(=O)OC1=CC=CC=C1C(=O)O', 'inchikey': 'BSYNRYMUTXBXSQ-UHFFFAOYSA-N'}
-        self.quinine = {'cas_id': '130-95-0', 'smiles': 'COC1=CC2=C(C=CN=C2C=C1)C(C3CC4CCN3CC4C=C)O', 'inchikey': 'LOUPRKONTZGTKE-WZBLMQSHSA-N'}
+        self.aspirin = {'CAS ID': '50-78-2', 'SMILES': 'CC(=O)Oc1ccccc1C(O)=O', 'InChIKey': 'BSYNRYMUTXBXSQ-UHFFFAOYSA-N'}
+        self.arte = {'CAS ID': '63968-64-9', 'SMILES': 'C[C@@H]1CC[C@H]2[C@@H](C)C(=O)O[C@@H]3O[C@@]4(C)CC[C@@H]1[C@@]23OO4',
+                     'InChIKey': 'BLUAFEHZUWYNDE-NNWCWBAJSA-N'}
+        self.artem = {'CAS ID': '71963-77-4', 'SMILES': 'CO[C@H]1O[C@@H]2O[C@@]3(C)CC[C@H]4[C@H](C)CC[C@@H]([C@H]1C)[C@@]24OO3',
+                      'InChIKey': 'SXYIRMFQILZOAM-HVNFFKDJSA-N'}
 
     def test_resolve_cas_to_smiles(self):
-        result = resolve_cas_to_smiles(self.quinine['cas_id'])
+        result = resolve_cas_to_smiles(self.artem['CAS ID'])
 
-        self.assertEqual(result, self.quinine['smiles'])
-        result = resolve_cas_to_smiles(self.aspirin['cas_id'])
+        self.assertEqual(result, self.artem['SMILES'])
 
-        self.assertEqual(result, self.aspirin['smiles'])
+        result = resolve_cas_to_smiles(self.arte['CAS ID'])
+
+        self.assertEqual(result, self.arte['SMILES'])
+        result = resolve_cas_to_smiles(self.aspirin['CAS ID'])
+
+        self.assertEqual(result, self.aspirin['SMILES'])
 
     def test_resolve_cas_to_inchikey(self):
-        result = resolve_cas_to_inchikey(self.aspirin['cas_id'])
+        result = resolve_cas_to_inchikey(self.artem['CAS ID'])
 
-        self.assertEqual(result, self.aspirin['inchikey'])
+        self.assertEqual(result, self.artem['InChIKey'])
 
-        result = resolve_cas_to_inchikey(self.quinine['cas_id'])
+        result = resolve_cas_to_inchikey(self.aspirin['CAS ID'])
 
-        self.assertEqual(result, self.quinine['inchikey'])
+        self.assertEqual(result, self.aspirin['InChIKey'])
+
+        result = resolve_cas_to_inchikey(self.arte['CAS ID'])
+
+        self.assertEqual(result, self.arte['InChIKey'])
+
+    def test_list_resolve(self):
+        result = get_smiles_and_inchi_from_cas_ids([self.aspirin['CAS ID'], self.arte['CAS ID'], self.artem['CAS ID']], 'temp_outputs')
+        correct_df = pd.DataFrame([self.aspirin, self.arte, self.artem])
+        pd.testing.assert_frame_equal(result, correct_df)
+
+    def test_df_resolve(self):
+        correct_df = pd.DataFrame([self.aspirin, self.arte, self.artem,self.aspirin])
+        result = add_CAS_ID_translations_to_df(correct_df[['CAS ID']], 'CAS ID','temp_outputs')
+        pd.testing.assert_frame_equal(result, correct_df)
 
 
 class Testkeys(unittest.TestCase):
