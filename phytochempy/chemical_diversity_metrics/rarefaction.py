5fd80fddc3c5987146bd8e46acf2f985fd10083b
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2024-11-09 14:22:12 +0000

    Improve rarefaction methods

--- phytochempy/chemical_diversity_metrics/rarefaction.py
+++ phytochempy/chemical_diversity_metrics/rarefaction.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from phytochempy.chemical_diversity_metrics import calculate_FAD_measures, get_pathway_based_diversity_measures
+
 
 def rarefy_diversity_for_group(df: pd.DataFrame, compound_grouping: str, group: str, target_size: int, metrics: list, method_to_calculate,
                                iterations=1000,
@@ -7,7 +9,7 @@ def rarefy_diversity_for_group(df: pd.DataFrame, compound_grouping: str, group: 
     diversity_calc_means = {}
     diversity_calc_stds = {}
     group_df = df[df[compound_grouping] == group]
-    bootstrap_df = pd.DataFrame()
+    bootstrap_results = []
     for _ in range(iterations):
         # Randomly subsample to the target size
         subsample = group_df.sample(target_size, replace=False)
@@ -16,12 +18,18 @@ def rarefy_diversity_for_group(df: pd.DataFrame, compound_grouping: str, group: 
         if compound_id_col is None:
             distances = method_to_calculate(subsample, compound_grouping)
         else:
-            distances = method_to_calculate(subsample, compound_grouping, compound_id_col)  # pdist returns condensed distance matrix
+            distances = method_to_calculate(subsample, compound_grouping, compound_id_col)
 
-        assert len(distances) == 1
-        bootstrap_df = pd.concat([bootstrap_df, distances])
+        if compound_id_col is None:
+            assert len(distances) == 1
+        else:
+            assert len(distances) <= 1
 
-    assert len(bootstrap_df) == iterations
+        bootstrap_results.append(distances)
+    bootstrap_df = pd.concat(bootstrap_results)
+
+    if compound_id_col is None:
+        assert len(bootstrap_df) == iterations
 
     for m in metrics:
         mean_distance = bootstrap_df[m].mean()
@@ -32,3 +40,21 @@ def rarefy_diversity_for_group(df: pd.DataFrame, compound_grouping: str, group: 
     diversity_calc_stds[compound_grouping] = group
     # Return the mean and standard deviation of diversity calcs over iterations
     return diversity_calc_means, diversity_calc_stds
+
+
+def compile_rarified_calculations(df: pd.DataFrame, compound_grouping: str, target_size: int, compound_id_col: str, iterations=1000):
+    fad_all_groups = pd.DataFrame()
+    pathway_all_groups = pd.DataFrame()
+    for group in df[compound_grouping].unique():
+        fad_means, fad_stds = rarefy_diversity_for_group(df, compound_grouping, group, target_size, ['FAD', 'MFAD', 'APWD'], calculate_FAD_measures,
+                                                         iterations=iterations)
+        group_df = pd.DataFrame(fad_means, index=[group])
+        fad_all_groups = pd.concat([fad_all_groups, group_df])
+
+        pathway_means, pathway_stds = rarefy_diversity_for_group(df, compound_grouping, group, target_size, ['H', 'Hbc', 'G', 'J'],
+                                                                 get_pathway_based_diversity_measures,
+                                                                 iterations=iterations, compound_id_col=compound_id_col)
+        pathway_group_df = pd.DataFrame(pathway_means, index=[group])
+        pathway_all_groups = pd.concat([pathway_all_groups, pathway_group_df])
+
+    return fad_all_groups, pathway_all_groups
--- phytochempy/chemical_diversity_metrics/unittests/rarefactions.py
+++ phytochempy/chemical_diversity_metrics/unittests/rarefactions.py
@@ -5,7 +5,7 @@ from operator import index
 import pandas as pd
 
 from phytochempy.chemical_diversity_metrics import add_pathway_information_columns, get_group_level_version_for_all_pathways, \
-    get_pathway_based_diversity_measures, NP_PATHWAYS, rarefy_diversity_for_group
+    get_pathway_based_diversity_measures, NP_PATHWAYS, rarefy_diversity_for_group, compile_rarified_calculations
 from phytochempy.chemical_diversity_metrics.compound_distance_metrics import _get_pairwise_distances_from_data, calculate_FAD_measures
 from phytochempy.compound_properties import get_npclassifier_classes_from_df
 
@@ -13,7 +13,8 @@ from phytochempy.compound_properties import get_npclassifier_classes_from_df
 class MyTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.df = pd.DataFrame({'Standard_SMILES': ['C(C)O', 'CO', 'C(C)N', 'C', 'CO'], 'Groups': ['A', 'A', 'A', 'B', 'B']})
+        df = pd.DataFrame({'Standard_SMILES': ['C(C)O', 'CO', 'C(C)N', 'C', 'CO'], 'Groups': ['A', 'A', 'A', 'B', 'B']})
+        self.df = get_npclassifier_classes_from_df(df, 'Standard_SMILES', 'inputs')
 
     def test_multiple(self):
         rarified_A = rarefy_diversity_for_group(self.df, 'Groups', 'A', 2, ['FAD', 'MFAD', 'APWD'], calculate_FAD_measures)
@@ -22,20 +23,42 @@ class MyTestCase(unittest.TestCase):
         AFADS = FADs[FADs['Groups'] == 'A']
         BFADS = FADs[FADs['Groups'] == 'B']
 
-        assert round(rarified_A[0]['APWD'], 2) == round(AFADS['APWD'].iloc[0], 2)  # APWD is similar when target size is similar to group size
+        assert round(rarified_A[0]['APWD_Rare'], 2) == round(AFADS['APWD'].iloc[0], 2)  # APWD is similar when target size is similar to group size
 
         rarified_B = rarefy_diversity_for_group(self.df, 'Groups', 'B', 2, ['FAD', 'MFAD', 'APWD'], calculate_FAD_measures)
-        assert round(rarified_B[0]['APWD'], 2) == round(BFADS['APWD'].iloc[0], 2)  # APWD is similar when target size is similar to group size
-        assert round(rarified_B[0]['FAD'], 2) == round(BFADS['FAD'].iloc[0], 2)  # FAD is similar when target size is same as group size
+        assert round(rarified_B[0]['APWD_Rare'], 2) == round(BFADS['APWD'].iloc[0], 2)  # APWD is similar when target size is similar to group size
+        assert round(rarified_B[0]['FAD_Rare'], 2) == round(BFADS['FAD'].iloc[0], 2)  # FAD is similar when target size is same as group size
 
         print(AFADS)
+
+    def test_pways(self):
+
+        rarified_A = rarefy_diversity_for_group(self.df, 'Groups', 'A', 3, ['H', 'Hbc', 'G', 'J'], get_pathway_based_diversity_measures,
+                                                compound_id_col='Standard_SMILES', iterations=10)
+
+        FADs = get_pathway_based_diversity_measures(self.df, 'Groups','Standard_SMILES')
+        AFADS = FADs[FADs['Groups'] == 'A']
+        BFADS = FADs[FADs['Groups'] == 'B']
+
+        assert round(rarified_A[0]['H_Rare'], 2) == round(AFADS['H'].iloc[0], 2)
+
+
+        # H is similar when target size is similar to group size
+        try:
+            rarified_B = rarefy_diversity_for_group(self.df, 'Groups', 'B', 3, ['H', 'Hbc', 'G', 'J'], get_pathway_based_diversity_measures,
+                                                    compound_id_col='Standard_SMILES', iterations=10)
+        except ValueError:
+            print('ok')
+        else:
+            raise ValueError
+
     def test_compiling(self):
-        all_groups = pd.DataFrame()
-        for group in self.df['Groups'].unique():
-            fad_means, fad_stds = rarefy_diversity_for_group(self.df,  'Groups', group, 2, ['FAD', 'MFAD', 'APWD'], calculate_FAD_measures)
-            group_df = pd.DataFrame(fad_means, index=[group])
-            all_groups = pd.concat([all_groups, group_df])
-            print(fad_means)
+        fad_all_groups, pathway_all_groups = compile_rarified_calculations(self.df, 'Groups',2, 'Standard_SMILES', 10)
+
+        assert len(fad_all_groups) == 2
+        assert len(pathway_all_groups) == 2
+        print(fad_all_groups)
+
 
 if __name__ == '__main__':
     unittest.main()
